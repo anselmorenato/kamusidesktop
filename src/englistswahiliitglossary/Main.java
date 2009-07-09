06a81a011f76757bda2490c7cfb0cb9b7b135f47
arthurbuliva <arthurbuliva@2fbacd0e-6c4d-11de-9a7c-6184c298a20a>
2009-07-09 06:27:14 +0000

    First source code release. Will add comments later on

---
+++ src/englistswahiliitglossary/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package englistswahiliitglossary;
+
+/**
+ *
+ * @author arthur
+ */
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        MainWindow mainWindow = new MainWindow();
+        
+    }
+}
