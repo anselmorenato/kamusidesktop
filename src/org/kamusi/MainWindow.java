e7fcc0873fbf74cb0a603ed544b223b47067794e
arthurbuliva <arthurbuliva@2fbacd0e-6c4d-11de-9a7c-6184c298a20a>
2009-07-22 05:59:53 +0000

    Added comments. Refactored code. Added new features.

---
+++ src/org/kamusi/MainWindow.java
@@ -0,0 +1,379 @@
+package org.kamusi;
+
+import java.awt.BorderLayout;
+import java.awt.Cursor;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.table.TableModel;
+
+/**
+ * This is the main window of the application
+ * @author arthur
+ */
+public class MainWindow extends JFrame
+{
+
+    /**
+     * The input field for the word to be translated
+     */
+    private JTextField wordField;
+    /**
+     * Label to direct for the input
+     */
+    private JLabel wordLabel;
+    /**
+     * Panel to hold the input field for the word
+     */
+    private JPanel wordInputPanel;
+    /**
+     * Panel to hold all the input objects
+     */
+    private JPanel inputPanel;
+    /**
+     * Panel to hold the checkboxes for the fields to be displayed
+     */
+    private JPanel fieldsPanel;
+    /**
+     * Panel for displaying the results
+     */
+    private JPanel outputPanel;
+    /**
+     * Panel for the status bar
+     */
+    private JPanel statusPanel;
+    /**
+     * The status label
+     */
+    private JLabel statusLabel, staticLabel;
+    /**
+     * Panel to hold the Radio Buttons for the available languages
+     */
+    private JPanel languagePanel;
+    /**
+     * Radio for Swahili to English translation
+     */
+    private JRadioButton swahiliToEnglish;
+    /**
+     * Radio for English to Swahili translation
+     */
+    private JRadioButton englishToSwahili;
+    /**
+     * Button group for the languages
+     */
+    private ButtonGroup language;
+    /**
+     * JTable for displaying output
+     */
+    private ResultTable resultTable;
+    /**
+     * Button for resetting the window
+     */
+    private JButton resetButton;
+    /**
+     * Checkboxes for the fields to be displayed
+     */
+    private JCheckBox swahiliWord;
+    private JCheckBox englishWord;
+    private JCheckBox englishExample;
+    private JCheckBox swahiliExample;
+    private JCheckBox englishPlural;
+    private JCheckBox swahiliPlural;
+
+    /**
+     * Initializes the display
+     */
+    public MainWindow()
+    {
+        super("Kamusi Project Desktop");
+        initComponents();
+        addActionListeners();
+    }
+
+    /**
+     * Sets the action listeners for the objects in the window
+     */
+    private void addActionListeners()
+    {
+        /**
+         * Adds action listener for the "clear" button
+         */
+        resetButton.addActionListener(new ActionListener()
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                reset(); // Reset the input/output display
+            }
+        });
+
+        /**
+         * Adds a key listener for looking up a word in real-time typing
+         */
+        wordField.addKeyListener(new KeyAdapter()
+        {
+
+            /**
+             * What happens when a key is released?
+             */
+            @Override
+            public void keyReleased(KeyEvent e)
+            {
+                fetchTranslation(e);
+            }
+        });
+    }
+
+    /**
+     * Initializes the components
+     */
+    private void initComponents()
+    {
+        wordLabel = new JLabel("Word", JLabel.LEFT);
+        wordField = new JTextField(70);
+        language = new ButtonGroup();
+        swahiliToEnglish = new JRadioButton("Swa to Eng", false);
+        englishToSwahili = new JRadioButton("Eng to Swa", true);
+        language.add(swahiliToEnglish);
+        language.add(englishToSwahili);
+        languagePanel = new JPanel();
+        languagePanel.setLayout(new GridLayout(0, 1));
+        languagePanel.add(englishToSwahili);
+        languagePanel.add(swahiliToEnglish);
+        resetButton = new JButton("RESET");
+
+        englishWord = new JCheckBox("English", true);
+        englishWord.setEnabled(false);
+        swahiliWord = new JCheckBox("Swahili", true);
+        swahiliWord.setEnabled(false);
+        englishExample = new JCheckBox("English Example");
+        swahiliExample = new JCheckBox("Swahili Example");
+        englishPlural = new JCheckBox("English Plural");
+        swahiliPlural = new JCheckBox("Swahili Plural");
+
+        fieldsPanel = new JPanel();
+        fieldsPanel.setLayout(new FlowLayout());
+        fieldsPanel.add(englishWord);
+        fieldsPanel.add(swahiliWord);
+        fieldsPanel.add(englishPlural);
+        fieldsPanel.add(swahiliPlural);
+        fieldsPanel.add(englishExample);
+        fieldsPanel.add(swahiliExample);
+
+        wordInputPanel = new JPanel();
+        wordInputPanel.setLayout(new FlowLayout());
+        wordInputPanel.add(languagePanel);
+        wordInputPanel.add(wordLabel);
+        wordInputPanel.add(wordField);
+        wordInputPanel.add(resetButton);
+
+        inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(0, 1));
+        inputPanel.add(wordInputPanel);
+        inputPanel.add(fieldsPanel);
+
+        outputPanel = new JPanel();
+        outputPanel.setLayout(new BorderLayout());
+
+        statusLabel = new JLabel("The Kamusi Project", JLabel.LEFT);
+        staticLabel = new JLabel("The Kamusi Project", JLabel.LEFT);
+        statusPanel = new JPanel();
+        statusPanel.setLayout(new BorderLayout());
+        statusPanel.add(statusLabel, BorderLayout.WEST);
+        statusPanel.add(staticLabel, BorderLayout.EAST);
+
+        setLayout(new BorderLayout());
+
+        add(inputPanel, BorderLayout.NORTH);
+        add(outputPanel, BorderLayout.CENTER);
+        add(statusPanel, BorderLayout.SOUTH);
+
+        setSize(500, 300);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+
+        try
+        {
+            // Set the look and feel to what the user's system looks like
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (ClassNotFoundException ex)
+        {
+            Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (InstantiationException ex)
+        {
+            Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (IllegalAccessException ex)
+        {
+            Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (UnsupportedLookAndFeelException ex)
+        {
+            Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        wordField.requestFocusInWindow(); // Makes the cursor go to this field on startup
+    }
+
+    /**
+     * Resets the window to its default state
+     */
+    private void reset()
+    {
+        wordField.setText("");
+        englishWord.setSelected(false);
+        englishExample.setSelected(false);
+        swahiliExample.setSelected(false);
+        englishPlural.setSelected(false);
+        swahiliPlural.setSelected(false);
+        outputPanel.removeAll();
+        updateStatusBar("The Kamusi Project");
+        pack();
+    }
+
+    /**
+     * Gets a Vector representation of the selected displayable fields
+     * @return The Vector representation
+     */
+    private Vector getDisplayableFields()
+    {
+        Vector fields = new Vector();
+
+        if (englishPlural.isSelected())
+        {
+            fields.add("EnglishPlural");
+        }
+        if (swahiliPlural.isSelected())
+        {
+            fields.add("SwahiliPlural");
+        }
+        if (englishExample.isSelected())
+        {
+            fields.add("EnglishExample");
+        }
+        if (swahiliExample.isSelected())
+        {
+            fields.add("SwahiliExample");
+        }
+        
+        return fields;
+    }
+
+    /**
+     * Displays a table containing the translations
+     * @param word the word to be translated
+     */
+    private void displayTable(String word)
+    {
+        String languageToTranslateFrom = "";
+        Vector fields = getDisplayableFields();
+
+        if (swahiliToEnglish.isSelected() || englishToSwahili.isSelected())
+        {
+
+            if (swahiliToEnglish.isSelected())
+            {
+                languageToTranslateFrom = "SWAHILI";
+            }
+            else if (englishToSwahili.isSelected())
+            {
+                languageToTranslateFrom = "ENGLISH";
+            }
+
+            if (word.trim().length() == 0)
+            {
+                JOptionPane.showMessageDialog(null, "Please input a word to translate first.");
+            }
+            else
+            {
+                outputPanel.removeAll();
+
+                resultTable = new ResultTable(languageToTranslateFrom, word, fields);
+                TableModel model = resultTable.getTableModel();
+
+                JTable newTable = new JTable(model);
+
+                JScrollPane scrollPane = new JScrollPane(newTable);
+
+                outputPanel.add(scrollPane, BorderLayout.CENTER);
+
+                // Update the status bar
+                updateStatusBar(resultTable.getResultCount() + " Rows fetched");
+
+                pack();
+            }
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, "Please select the desired language first");
+        }
+    }
+
+    /**
+     * Updates the status bar with the new status message
+     * @param newMessage the new message to be displayed
+     */
+    private void updateStatusBar(String newMessage)
+    {
+        statusLabel.setText(newMessage);
+    }
+
+    /**
+     * Fetches and displays the translation depending on the key pressed
+     * on the field bearing the word to translate
+     * @param e The key to listen for
+     */
+    private void fetchTranslation(KeyEvent e)
+    {
+        String word = wordField.getText().trim();
+
+        if (e.isActionKey())
+        {
+        }
+        else if (e.getKeyCode() == 8) // Backspace
+        {
+            if (word.length() == 0) // Backspace
+            {
+                reset();
+            }
+            else
+            {
+                setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+                displayTable(word);
+                setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+            }
+        }
+        else
+        {
+            setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+            displayTable(word);
+            setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+        }
+    }
+    //TODO: Implement database update mechanism
+}
---
+++ src/org/kamusi/ResultTable.java
@@ -0,0 +1,229 @@
+package org.kamusi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+public class ResultTable extends DefaultTableModel
+{
+    
+    /**
+     * The translation SQLite3 database
+     */
+    private String database = "jdbc:sqlite:kamusiproject.db";
+    /**
+     * Translation database username. Defaults to blank
+     */
+    private String username = "";
+    /**
+     * Translation database password. Defaults to blank
+     */
+    private String password = "";
+    /**
+     * Table to display the output
+     */
+    private JTable table;
+    /**
+     * Connection to the database
+     */
+    private Connection connection;
+    private PreparedStatement statement;
+    private ResultSet resultSet;
+    /**
+     * The fields available to be fetched
+     */
+    private String englishWord, swahiliWord, englishExample, swahiliExample, swahiliPlural,
+            englishPlural;
+    /**
+     * Holds the number of records fetched
+     */
+    private int row = 0;
+
+    /**
+     * Initializes the class
+     * @param fromLanguage the language from which we are translating
+     * @param word the word to translate
+     * @param fields the fields which we are interested in
+     */
+    public ResultTable(String fromLanguage, String word, Vector fields)
+    {
+        Vector heading = new Vector();
+        Vector data = new Vector();
+
+        String fieldsString = "";
+        Enumeration availableFieldsString = fields.elements();
+        while (availableFieldsString.hasMoreElements())
+        {
+            fieldsString += ((String) availableFieldsString.nextElement());
+        }
+
+        try
+        {
+            Class.forName("org.sqlite.JDBC").newInstance();
+            connection = DriverManager.getConnection(database, username, password);
+
+            String query = "";
+
+            if (fromLanguage.equalsIgnoreCase("ENGLISH"))
+            {
+                query = "select * from dict where EnglishSortBy like ? " +
+                        "Order by EnglishSortBy asc";
+                heading.addElement("English");
+                heading.addElement("Swahili");
+
+                Enumeration availableFields = fields.elements();
+                while (availableFields.hasMoreElements())
+                {
+                    heading.addElement((String) availableFields.nextElement());
+                }
+            }
+            else if (fromLanguage.equalsIgnoreCase("SWAHILI"))
+            {
+                query = "select * from dict where SwahiliSortBy like ? " +
+                        "Order by SwahiliSortBy asc";
+                heading.addElement("Swahili");
+                heading.addElement("English");
+
+                Enumeration availableFields = fields.elements();
+                while (availableFields.hasMoreElements())
+                {
+                    heading.addElement((String) availableFields.nextElement());
+                }
+            }
+            else
+            {
+                throw new Exception("Undefined language!");
+            }
+
+            statement = connection.prepareStatement(query);
+
+            statement.setString(1, "%" + word + "%");
+
+            resultSet = statement.executeQuery();
+
+            row = 0; // To hold the number of records
+
+            while (resultSet.next())
+            {
+                row++;
+
+                englishWord = resultSet.getString("EnglishSortBy");
+                swahiliWord = resultSet.getString("SwahiliSortBy");
+                englishPlural = resultSet.getString("EnglishPlural");
+                swahiliPlural = resultSet.getString("SwahiliPlural");
+                englishExample = resultSet.getString("EnglishExample");
+                swahiliExample = resultSet.getString("SwahiliExample");
+
+                //create a row
+                Vector fieldToDisplay = new Vector();
+
+                if (fromLanguage.equalsIgnoreCase("ENGLISH"))
+                {
+                    fieldToDisplay.addElement(englishWord);
+                    fieldToDisplay.addElement(swahiliWord);
+
+                    if (fieldsString.contains("EnglishPlural"))
+                    {
+                        fieldToDisplay.addElement(englishPlural);
+                    }
+                    if (fieldsString.contains("SwahiliPlural"))
+                    {
+                        fieldToDisplay.addElement(swahiliPlural);
+                    }
+                    if (fieldsString.contains("EnglishExample"))
+                    {
+                        fieldToDisplay.addElement(englishExample);
+                    }
+                    if (fieldsString.contains("SwahiliExample"))
+                    {
+                        fieldToDisplay.addElement(swahiliExample);
+                    }
+                }
+                else if (fromLanguage.equalsIgnoreCase("SWAHILI"))
+                {
+                    fieldToDisplay.addElement(swahiliWord);
+                    fieldToDisplay.addElement(englishWord);
+
+                    if (fieldsString.contains("EnglishPlural"))
+                    {
+                        fieldToDisplay.addElement(englishPlural);
+                    }
+                    if (fieldsString.contains("SwahiliPlural"))
+                    {
+                        fieldToDisplay.addElement(swahiliPlural);
+                    }
+                    if (fieldsString.contains("EnglishExample"))
+                    {
+                        fieldToDisplay.addElement(englishExample);
+                    }
+                    if (fieldsString.contains("SwahiliExample"))
+                    {
+                        fieldToDisplay.addElement(swahiliExample);
+                    }
+                }
+
+                //add to model
+                data.addElement(fieldToDisplay);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                resultSet.close();
+                statement.close();
+                connection.close();
+            }
+            catch (SQLException ex)
+            {
+                Logger.getLogger(ResultTable.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        this.table = new JTable(data, heading);
+
+        TableModel model = this.table.getModel();
+    }
+
+    /**
+     * Returns a JTable created from #getTableModel
+     * @return the JTable Object
+     */
+    public JTable getTable()
+    {
+        return this.table;
+    }
+
+    /**
+     * Returns a table model
+     * @return the table model
+     */
+    public TableModel getTableModel()
+    {
+        TableModel model = this.table.getModel();
+
+        return model;
+    }
+
+    /**
+     * Gets how many records have been fetched
+     * @return the number of records
+     */
+    public int getResultCount()
+    {
+        return this.row;
+    }
+}
