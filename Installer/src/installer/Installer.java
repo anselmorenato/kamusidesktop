56b4fad6c79a474dd4037ec9f71f97dc757d8b55
arthurbuliva <arthurbuliva@2fbacd0e-6c4d-11de-9a7c-6184c298a20a>
2009-09-22 12:19:36 +0000

    Adding installer code

---
+++ Installer/src/installer/Installer.java
@@ -0,0 +1,15 @@
+package installer;
+
+/**
+ *
+ * @author arthur
+ */
+public class Installer
+{
+    private ApplicationProperties properties;
+
+    public Installer(String installPath)
+    {
+           properties = new ApplicationProperties();
+    }
+}
