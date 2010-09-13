aa55636512fd7bfad32e7dfbfb7783a63956fab1
arthurbuliva <arthurbuliva@2fbacd0e-6c4d-11de-9a7c-6184c298a20a>
2010-09-13 09:43:55 +0000

    Adding NetBeans platform branch

---
+++ branch/Kamusi Desktop Platform/Dictionary/test/unit/src/org/kamusi/dictionary/Test.java
@@ -0,0 +1,19 @@
+/**
+ * Test.java
+ * Created on Aug 19, 2010, 11:22:40 AM
+ * @author arthur
+ */
+package org.kamusi.dictionary;
+
+
+/**
+ * class Test
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        DictionaryDownloader instance = new DictionaryDownloader();
+        instance.downloadDictionary();
+    }
+}
